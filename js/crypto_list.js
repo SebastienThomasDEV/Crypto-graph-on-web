32ae26173de5dbcfbfc4cc541e7347654569458a
SebastienThomasDEV <sebastienthomaspr@gmail.com>
2022-11-16 04:00:00 +0100

    started to make the form to choose cryptocurrency

---
+++ js/crypto_list.js
@@ -0,0 +1,402 @@
+export const crypto_list = [
+    {
+        "currency": "Bitcoin",
+        "abbreviation": "BTC"
+    },
+    {
+        "currency": "Ethereum",
+        "abbreviation": "ETH"
+    },
+    {
+        "currency": "Bitcoin Cash",
+        "abbreviation": "BCH"
+    },
+    {
+        "currency": "EOS",
+        "abbreviation": "EOS"
+    },
+    {
+        "currency": "XRP",
+        "abbreviation": "XRP"
+    },
+    {
+        "currency": "Litecoin",
+        "abbreviation": "LTC"
+    },
+    {
+        "currency": "Chainlink",
+        "abbreviation": "LINK"
+    },
+    {
+        "currency": "TRON",
+        "abbreviation": "TRX"
+    },
+    {
+        "currency": "Monero",
+        "abbreviation": "XMR"
+    },
+    {
+        "currency": "Binance Coin",
+        "abbreviation": "BNB"
+    },
+    {
+        "currency": "NEO",
+        "abbreviation": "NEO"
+    },
+    {
+        "currency": "Ethereum Classic",
+        "abbreviation": "ETC"
+    },
+    {
+        "currency": "Bitcoin SV",
+        "abbreviation": "BSV"
+    },
+    {
+        "currency": "VeChain",
+        "abbreviation": "VET"
+    },
+    {
+        "currency": "Force Protocol",
+        "abbreviation": "FOR"
+    },
+    {
+        "currency": "Ontology",
+        "abbreviation": "ONT"
+    },
+    {
+        "currency": "True Chain",
+        "abbreviation": "TRUE"
+    },
+    {
+        "currency": "Tether",
+        "abbreviation": "USDT"
+    },
+    {
+        "currency": "Stellar",
+        "abbreviation": "XLM"
+    },
+    {
+        "currency": "QTUM",
+        "abbreviation": "QTUM"
+    },
+    {
+        "currency": "Basic Attention Token",
+        "abbreviation": "BAT"
+    },
+    {
+        "currency": "OmiseGo",
+        "abbreviation": "OMG"
+    },
+    {
+        "currency": "Cosmos",
+        "abbreviation": "ATOM"
+    },
+    {
+        "currency": "USD Coin",
+        "abbreviation": "USDC"
+    },
+    {
+        "currency": "Cardano",
+        "abbreviation": "ADA"
+    },
+    {
+        "currency": "Kucoin",
+        "abbreviation": "KCS"
+    },
+    {
+        "currency": "Tezos",
+        "abbreviation": "XTZ"
+    },
+    {
+        "currency": "True USD",
+        "abbreviation": "TUSD"
+    },
+    {
+        "currency": "Chiliz",
+        "abbreviation": "CHZ"
+    },
+    {
+        "currency": "Lambda",
+        "abbreviation": "LAMB"
+    },
+    {
+        "currency": "Fetch.AI",
+        "abbreviation": "FET"
+    },
+    {
+        "currency": "Paxos Standard",
+        "abbreviation": "PAX"
+    },
+    {
+        "currency": "Algorand",
+        "abbreviation": "ALGO"
+    },
+    {
+        "currency": "IOS token",
+        "abbreviation": "IOST"
+    },
+    {
+        "currency": "IOTA",
+        "abbreviation": "MIOTA"
+    },
+    {
+        "currency": "Cortex",
+        "abbreviation": "CTXC"
+    },
+    {
+        "currency": "Crypto.com Chain Token",
+        "abbreviation": "CRO"
+    },
+    {
+        "currency": "Huobi Token",
+        "abbreviation": "HT"
+    },
+    {
+        "currency": "Wirex Token",
+        "abbreviation": "WXT"
+    },
+    {
+        "currency": "Measurable Data Token",
+        "abbreviation": "MSDT"
+    },
+    {
+        "currency": "Dash",
+        "abbreviation": "DASH"
+    },
+    {
+        "currency": "Bytom",
+        "abbreviation": "BTM"
+    },
+    {
+        "currency": "Theta",
+        "abbreviation": "THETA"
+    },
+    {
+        "currency": "ZCash",
+        "abbreviation": "ZEC"
+    },
+    {
+        "currency": "Crypto.com",
+        "abbreviation": "MCO"
+    },
+    {
+        "currency": "BitTorrent",
+        "abbreviation": "BTT"
+    },
+    {
+        "currency": "ARPA Chain",
+        "abbreviation": "ARPA"
+    },
+    {
+        "currency": "Egretia",
+        "abbreviation": "EGT"
+    },
+    {
+        "currency": "0x",
+        "abbreviation": "ZRX"
+    },
+    {
+        "currency": "Matic Network",
+        "abbreviation": "MATIC"
+    },
+    {
+        "currency": "ArcBlock",
+        "abbreviation": "ABT"
+    },
+    {
+        "currency": "Decentraland",
+        "abbreviation": "MANA"
+    },
+    {
+        "currency": "Nebulas",
+        "abbreviation": "NAS"
+    },
+    {
+        "currency": "NKN",
+        "abbreviation": "NKN"
+    },
+    {
+        "currency": "Ravencoin",
+        "abbreviation": "RVN"
+    },
+    {
+        "currency": "Tierion",
+        "abbreviation": "TNT"
+    },
+    {
+        "currency": "ICON Project",
+        "abbreviation": "ICX"
+    },
+    {
+        "currency": "Bitcoin BEP2",
+        "abbreviation": "BTCB"
+    },
+    {
+        "currency": "Hedera Hashgraph",
+        "abbreviation": "HBAR"
+    },
+    {
+        "currency": "CyberMiles",
+        "abbreviation": "CMT"
+    },
+    {
+        "currency": "Populous",
+        "abbreviation": "PPT"
+    },
+    {
+        "currency": "Nano",
+        "abbreviation": "NANO"
+    },
+    {
+        "currency": "Eminer",
+        "abbreviation": "EM"
+    },
+    {
+        "currency": "Zilliqa",
+        "abbreviation": "ZIL"
+    },
+    {
+        "currency": "Dai",
+        "abbreviation": "DAI"
+    },
+    {
+        "currency": "Ethos",
+        "abbreviation": "ETHOS"
+    },
+    {
+        "currency": "DxChain Token",
+        "abbreviation": "DX"
+    },
+    {
+        "currency": "SwftCoin",
+        "abbreviation": "SWFTC"
+    },
+    {
+        "currency": "Viberate",
+        "abbreviation": "VIB"
+    },
+    {
+        "currency": "IoT Chain",
+        "abbreviation": "ITC"
+    },
+    {
+        "currency": "FTX Token",
+        "abbreviation": "FTXT"
+    },
+    {
+        "currency": "NEM",
+        "abbreviation": "XEM"
+    },
+    {
+        "currency": "Ambrosus",
+        "abbreviation": "AMB"
+    },
+    {
+        "currency": "MX Token",
+        "abbreviation": "MX"
+    },
+    {
+        "currency": "AERGO",
+        "abbreviation": "AERGO"
+    },
+    {
+        "currency": "TomoChain",
+        "abbreviation": "TOMO"
+    },
+    {
+        "currency": "Beam",
+        "abbreviation": "BEAM"
+    },
+    {
+        "currency": "RadonPay",
+        "abbreviation": "RDN"
+    },
+    {
+        "currency": "Metal",
+        "abbreviation": "MTL"
+    },
+    {
+        "currency": "Komodo",
+        "abbreviation": "KMD"
+    },
+    {
+        "currency": "GIFTO",
+        "abbreviation": "GTO"
+    },
+    {
+        "currency": "Bitcoin Gold",
+        "abbreviation": "BTG"
+    },
+    {
+        "currency": "Enigma",
+        "abbreviation": "ENG"
+    },
+    {
+        "currency": "Elrond",
+        "abbreviation": "ERD"
+    },
+    {
+        "currency": "Enjin Coin",
+        "abbreviation": "ENJ"
+    },
+    {
+        "currency": "Civic",
+        "abbreviation": "CVC"
+    },
+    {
+        "currency": "NEXO",
+        "abbreviation": "NEXO"
+    },
+    {
+        "currency": "Aeternity",
+        "abbreviation": "AE"
+    },
+    {
+        "currency": "Loom Network",
+        "abbreviation": "LOOM"
+    },
+    {
+        "currency": "EthLend",
+        "abbreviation": "LEND"
+    },
+    {
+        "currency": "Augur",
+        "abbreviation": "REP"
+    },
+    {
+        "currency": "Storm",
+        "abbreviation": "STORM"
+    },
+    {
+        "currency": "Exchange Union",
+        "abbreviation": "XUC"
+    },
+    {
+        "currency": "Nuls",
+        "abbreviation": "NULS"
+    },
+    {
+        "currency": "Holo",
+        "abbreviation": "HOLO"
+    },
+    {
+        "currency": "GXChain",
+        "abbreviation": "GXS"
+    },
+    {
+        "currency": "Waves",
+        "abbreviation": "WAVES"
+    },
+    {
+        "currency": "Private Instant Verified Transaction",
+        "abbreviation": "PIVX"
+    },
+    {
+        "currency": "Tael",
+        "abbreviation": "WABI"
+    },
+    {
+        "currency": "Waltonchain",
+        "abbreviation": "WTC"
+    }
+]
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
+import {crypto_list} from './crypto_list.js';
 
-
-async function getData() {
-    let response1 = await fetch('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=EUR')
+async function getData(choice) {
+    // let url = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${choice}&tsyms=EUR;`);
+    let response1 = await fetch('https://min-api.cryptocompare.com/data/price?fsym=KCS&tsyms=EUR')
     let data_graph = [response1.json()]
     return data_graph
 }
@@ -24,7 +25,8 @@ function alea(min, max) { // min and max included
 function build_graph(nb_graph) {
     const graph = document.createElement("canvas"); //créer un element HTML => canvas
     graph.setAttribute('id',`Chart${nb_graph}`); //donne un id à l'element HTML => id incrémentable
-    const choice = "BTC"
+    graph.classList.add("graph")
+    const choice = "KCS"
     const config = config_graph(choice)
     const builded = new Chart(graph,config);
     $('.add_crypto').append(graph);
@@ -33,27 +35,70 @@ function build_graph(nb_graph) {
 }
   
 function config_graph(choice) {
+    // Choisi une couleur aléatoire
     let color = [alea(0, 255),alea(0, 255),alea(0, 255)]
+    // Config pour les labels du graphs(heures)
     const labels = []
+     // Config pour les datas du graphs(legende + data)
     const data = {
         labels: labels,
         datasets: [{
+            // Donne un nom à la legende du graph par rapport au choix de l'utilisateur
             label: `${choice}/EUR`,
+            // Donne une couleur aleatoire
             backgroundColor: `rgb(${color[0]},${color[1]},${color[2]})`,
             borderColor: `rgb(${color[0]},${color[1]},${color[2]})`,
+            // Array qui contient les datas pour construires le graph
             data: [],
         }]
     }
     
     const config = {
+        // Définit le type de graph
         type: 'line',
+        // ajoute les configs
         data: data,
+        // options pour personnaliser son graph (voir doc)
         options: {}
     }
-    
+    // retourne la config du graph
     return config
 }
 
+
+function slider_coin(list) {
+    // /////////////////////////////
+
+    let values = list
+    // /////////////////////////////
+    let list_wpr = document.createElement("div");
+    list_wpr.classList.add("list_wpr")
+
+    let search_cryptos = document.createElement("input")
+    search_cryptos.classList.add("search")
+    search_cryptos.setAttribute('placeholder',`search`);
+    $(list_wpr).append(search_cryptos);
+
+    let userList = new List('cryptos_list', values);
+    const list_coin = document.createElement("ul");
+    list_coin.setAttribute('id',`cryptos_wpr`);
+
+    list.forEach(element => {
+        let coin_wpr = document.createElement("li");
+        let coin = document.createElement("button");
+        $(coin_wpr).append(coin); 
+        coin.setAttribute('id',`coins`);
+        let text = document.createTextNode(`${element.currency}-${element.abbreviation}`);
+        coin.appendChild(text);
+        $(list_coin).append(coin_wpr);
+        $(list_wpr).append(coin_wpr);
+    });
+    console.log(list_wpr)
+    swal({
+        title: "Choisi un crypto",
+        content: list_wpr,
+      });
+}
 
 
 
@@ -65,6 +110,7 @@ const add_btn = document.querySelector(".add_btn");
 let nb_graph = 0
 
 add_btn.addEventListener('click', () => {
+    slider_coin(crypto_list)
     build_graph(nb_graph)
     nb_graph++
 })
@@ -92,13 +138,7 @@ close_btn.addEventListener('click', () => {
     $(".page_title").hide();
 })
 
-let crypto = [];
-let choice = '';
-let url = `https://min-api.cryptocompare.com/data/price?fsym=${choice}&tsyms=EUR;`;
 
-
-
-    
 
 
 
